dc1fb037e28ee655d925bbb6d4ac8194fd3f68f1
Janelle Berscheid <25212807+oitoku@users.noreply.github.com>
2019-07-01 15:25:11 -0600

    Implemented function to save grid of results as comparison.

--- trainer.py
+++ trainer.py
@@ -3,7 +3,8 @@ from tensorflow.keras.callbacks import EarlyStopping
 import numpy as np
 import os
 import models
-import PIL
+from PIL import Image
+import matplotlib.pyplot as plt
 
 def load_data(directory):
 	# Open dir and grab all PNG, assert all have same dimensions
@@ -13,8 +14,10 @@ def load_data(directory):
 		for index, f in enumerate(files):
 			if index % 100 == 0:
 				print(index, end='\r')
+				if index > 500:
+					break
 			if f.split(".")[-1] == "png":
-				im = np.asarray(PIL.Image.open(directory + "/" + f))
+				im = np.asarray(Image.open(directory + "/" + f))
 				if len(im.shape) == 2:
 					im = im.reshape(*im.shape, 1)
 				im = im / 255.0
@@ -24,9 +27,31 @@ def load_data(directory):
 		raise ValueError("Images must all have the same width and height.")
 	return images
 
-def save_result(images):
-	
+def save_result(filename, images, reconstructed, num=10, invert=False):
+	if num % 2 != 0:
+		raise ValueError("num must be an even number.")
 
+	if invert:
+		images = 1 - images
+		reconstructed = 1 - reconstructed
+
+	# create grid of images with alternating columns: original, reconstructed ...
+	length = images.shape[1]
+
+	imgs_per_output = int((num*num)/2)
+	output_image = np.zeros((length*num, length*num, images.shape[3]))
+	for i in range(imgs_per_output):
+		selected = np.random.randint(images.shape[0])
+		row = i % num
+		col = (i // num)*2
+		output_image[row*length: (row+1)*length , col*length: (col+1)*length] = images[selected, :, :, :]
+		output_image[row*length: (row+1)*length , (col+1)*length: (col+2)*length] = reconstructed[selected, :, :, :]
+
+	output_image = np.round(output_image * 255)
+	output_image = output_image.astype(np.uint8)
+	output_image = output_image.reshape((length*num, length*num))
+	output_image = Image.fromarray(output_image, mode="L")
+	output_image.save(filename, mode="L")
 
 def train_model(model, data):
 	model.fit(data, data, epochs=1000, validation_split=0.2, callbacks=[EarlyStopping(patience=5)], batch_size=128)
@@ -41,4 +66,5 @@ if __name__ == "__main__":
 
 	data = load_data(os.getcwd() + "/data/mnist")
 
-	train_model(model, data)
+	save_result("output_comparison.png", data, data, invert=True)
+	#train_model(model, data)
